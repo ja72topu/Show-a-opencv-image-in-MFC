ecd26fd57243a0cf26eff29e1878a7e831e31c1e
ja72topu <34000594+ja72topu@users.noreply.github.com>
2017-12-02 16:21:53 +0100

    Update how_to_use.cpp

--- how_to_use.cpp
+++ how_to_use.cpp
@@ -1,4 +1,4 @@
-Cstring images;// from "add variable" by click picture control in MFC dialog. 
+Cstatic images;// from "add variable" by click picture control in MFC dialog. 
 cv::Mat cvImg=imread("C:\\Users\\mango\\Desktop\\c++\\imageread\\x64\\Release\\3.bmp");
 CvMatToWinControl(cvImg, &images); // don't forget this symbol "&".
                                    // then you can see the images in picture box
